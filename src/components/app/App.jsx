b840b91fcb2f16177f1d3ff406bb04ee0b67b11b
Jonathan Sarco <jfe.sarco@gmail.com>
2021-04-05 07:46:44 -0300

    change position of footer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <p>Something to write about...</p>
     <p>Something to write about...</p>
     <p>Something to write about...</p>
+    {/* <p>Something to write about...</p>
     <p>Something to write about...</p>
     <p>Something to write about...</p>
     <p>Something to write about...</p>
@@ -37,8 +38,7 @@ function App() {
     <p>Something to write about...</p>
     <p>Something to write about...</p>
     <p>Something to write about...</p>
-    <p>Something to write about...</p>
-    <p>Something to write about...</p>
+    <p>Something to write about...</p> */}
     </>
   )
 
